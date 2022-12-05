6cd50eaa2c2e642ffae831258b556ddd24fdc06d
maheedhar132 <maheedhartalluri@gmail.com>
2022-12-05 19:35:19 +0530

    provider info added

---
+++ provider.tf
@@ -0,0 +1,6 @@
+# The default provider configuration; resources that begin with `aws_` will use
+# it as the default, and it can be referenced as `aws`.
+provider "aws" {
+  region = "ap-south-1"
+}
+
